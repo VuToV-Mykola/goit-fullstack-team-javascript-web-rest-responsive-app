ca45367d35f3c974eb21d09820034981d8c4725e
VuToV-Mykola <vutov_nikola@icloud.com>
2026-02-05 12:44:03 +0100

    Add main.js imports for all src/js modules

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
+import "./js/refs.js";
+import "./js/api-artists.js";
+import "./js/modal.js";
+import "./js/artist-modal.js";
+import "./js/feedback.js";
+import "./js/feedback-modal.js";
+import "./js/header-mob-menu.js";
 import "./js/hero.js";
