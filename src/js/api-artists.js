8d594ed36271ba3e8afe33867be6c4aeb6082979
Tetiana Zinovieva <talia.97.it@gmail.com>
2026-02-05 22:18:55 +0100

    artists section, api and not finaly css

---
+++ src/js/api-artists.js
@@ -0,0 +1,75 @@
+import axios from "axios";
+
+const API_URL = 'https://sound-wave.b.goit.study/api/artists';
+const PER_PAGE = 8;
+
+const list = document.getElementById('artistsList');
+const loadMoreBtn = document.getElementById('loadMoreBtn');
+const loader = document.getElementById('artistsLoader');
+
+let page = 1;
+
+async function fetchArtists() {
+    try {
+        showLoader();
+
+        const response = await fetch(
+            `${API_URL}?page=${page}&limit=${PER_PAGE}`
+        );
+
+        if (!response.ok) {
+            throw new Error('Failed to fetch artists');
+        }
+
+        const data = await response.json();
+
+        renderArtists(data.artists);
+
+        if (data.data.length < PER_PAGE) {
+            loadMoreBtn.classList.add('is-hidden');
+        } else {
+            loadMoreBtn.classList.remove('is-hidden');
+        }
+
+        page += 1;
+    } catch (error) {
+        console.error(error);
+    } finally {
+        hideLoader();
+    }
+    
+}
+
+
+
+export function renderArtists(artists) {
+  if (!Array.isArray(artists)) return;
+
+  const markup = artists
+    .map(({ _id, strArtist, strArtistThumb, genres, strBiographyEN}) => {
+      return `
+        <li class="artist-card" data-id="${_id}">
+          <img src="${strArtistThumb}" alt="${strArtist}" class="artist-img" />
+          <p class="artist-genres">${genres.join(', ')}</p>
+          <h3 class="artist-name">${strArtist}</h3>
+          <p class="artist-bio">${strBiographyEN}</p>
+          <button class="artist-more">Learn more</button>
+        </li>
+      `;
+    })
+    .join('');
+
+  artistsList.insertAdjacentHTML('beforeend', markup);
+}
+
+function showLoader() {
+    loader.classList.remove('is-hidden');
+}
+
+function hideLoader() {
+    loader.classList.add('is-hidden');
+}
+
+loadMoreBtn.addEventListener('click', fetchArtists);
+
+fetchArtists();
