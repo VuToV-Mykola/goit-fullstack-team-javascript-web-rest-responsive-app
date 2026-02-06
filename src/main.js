a0fa1df1fba1e5d1b54f76b35bcc3dece3da8491
VuToV-Mykola <vutov_nikola@icloud.com>
2026-02-06 19:39:59 +0100

    add artists.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
-import "./js/refs.js";
-import "./js/api-artists.js";
-import "./js/modal.js";
-import "./js/artist-modal.js";
-import "./js/feedback.js";
-import "./js/feedback-modal.js";
-import "./js/header-mob-menu.js";
-import "./js/hero.js";
+import './js/config.js';
+import './js/api-artists.js';
+
+import './js/header-mob-menu.js';
+import './js/hero.js';
+import './js/artists.js';
+import './js/artist-modal.js';
