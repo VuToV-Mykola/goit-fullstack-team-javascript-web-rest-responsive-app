c233b7ade3d99eaa6401cc688221ffc6504498b3
VuToV-Mykola <vutov_nikola@icloud.com>
2026-02-07 14:37:29 +0100

    fix arrows in feedback section

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ import './js/api-artists.js';
 import './js/header-mob-menu.js';
 import './js/hero.js';
 import './js/artists.js';
-import './js/artist-modal.js';
-import './js/artist-modal-list.js';
+/* import './js/artist-modal.js';
+import './js/artist-modal-list.js'; */
 import './js/feedback.js';
