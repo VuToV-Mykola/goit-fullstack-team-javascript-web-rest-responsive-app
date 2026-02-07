465500da985bed5b98b554ad8d6ee2eeb48a7bb7
VuToV-Mykola <vutov_nikola@icloud.com>
2026-02-07 19:46:04 +0000

    Deploying to gh-pages from @ VuToV-Mykola/goit-fullstack-team-javascript-web-rest-responsive-app@a3c6fc5de7957410630f2b309cb6ac6f236a42be 🚀

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-import{a as v}from"./assets/vendor-D5GkNzM3.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const s of document.querySelectorAll('link[rel="modulepreload"]'))a(s);new MutationObserver(s=>{for(const r of s)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&a(o)}).observe(document,{childList:!0,subtree:!0});function n(s){const r={};return s.integrity&&(r.integrity=s.integrity),s.referrerPolicy&&(r.referrerPolicy=s.referrerPolicy),s.crossOrigin==="use-credentials"?r.credentials="include":s.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function a(s){if(s.ep)return;s.ep=!0;const r=n(s);fetch(s.href,r)}})();const y="https://sound-wave.b.goit.study/api",A=1,$=8;async function M(t=A,e=$){return(await v.get(`${y}/artists`,{params:{page:t,limit:e}})).data}async function I(t){if(!t)throw new Error("Artist id is required");return(await v.get(`${y}/artists/${t}`)).data}document.querySelectorAll(".nav-list a").forEach(t=>{t.addEventListener("click",()=>{document.getElementById("nav-toggle").checked=!1})});function B(){const t=document.querySelector("#btn-hero-js");t&&t.addEventListener("click",()=>{const e=document.querySelector("#artists");e&&e.scrollIntoView({behavior:"smooth",block:"start"})})}document.addEventListener("DOMContentLoaded",()=>{B()});document.getElementById("artistsList");const l=document.getElementById("loadMoreBtn"),L=document.getElementById("artistsLoader");let f=1;const p=8;async function g(){try{k();const t=await M(f,p),e=t.artists||t;S(e),e.length<p?l.classList.add("is-hidden"):l.classList.remove("is-hidden"),f+=1}catch(t){console.error("Failed to load artists:",t)}finally{T()}}function S(t){if(!Array.isArray(t))return;const e=t.map(({_id:n,strArtist:a,strArtistThumb:s,genres:r,strBiographyEN:o})=>{const c=r.map(d=>`<span class="artist-genre">${d}</span>`).join("");return`
-        <li class="artist-card" data-id="${n}">
+import{a as v}from"./assets/vendor-D5GkNzM3.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const s of document.querySelectorAll('link[rel="modulepreload"]'))n(s);new MutationObserver(s=>{for(const r of s)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&n(o)}).observe(document,{childList:!0,subtree:!0});function a(s){const r={};return s.integrity&&(r.integrity=s.integrity),s.referrerPolicy&&(r.referrerPolicy=s.referrerPolicy),s.crossOrigin==="use-credentials"?r.credentials="include":s.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function n(s){if(s.ep)return;s.ep=!0;const r=a(s);fetch(s.href,r)}})();const y="https://sound-wave.b.goit.study/api",A=1,$=8;async function M(t=A,e=$){return(await v.get(`${y}/artists`,{params:{page:t,limit:e}})).data}async function I(t){if(!t)throw new Error("Artist id is required");return(await v.get(`${y}/artists/${t}`)).data}document.querySelectorAll(".nav-list a").forEach(t=>{t.addEventListener("click",()=>{document.getElementById("nav-toggle").checked=!1})});function B(){const t=document.querySelector("#btn-hero-js");t&&t.addEventListener("click",()=>{const e=document.querySelector("#artists");e&&e.scrollIntoView({behavior:"smooth",block:"start"})})}document.addEventListener("DOMContentLoaded",()=>{B()});document.getElementById("artistsList");const l=document.getElementById("loadMoreBtn"),h=document.getElementById("artistsLoader");let f=1;const p=8;async function g(){try{k();const t=await M(f,p),e=t.artists||t;S(e),e.length<p?l.classList.add("is-hidden"):l.classList.remove("is-hidden"),f+=1}catch(t){console.error("Failed to load artists:",t)}finally{T()}}function S(t){if(!Array.isArray(t))return;const e=t.map(({_id:a,strArtist:n,strArtistThumb:s,genres:r,strBiographyEN:o})=>{const c=r.map(d=>`<span class="artist-genre">${d}</span>`).join("");return`
+        <li class="artist-card" data-id="${a}">
           <div class="artist-thumb">
-            <img src="${s}" alt="${a}" class="artist-img" />
+            <img src="${s}" alt="${n}" class="artist-img" />
           </div>
 
           <div class="artist-content">
             <div class="artist-genres">${c}</div>
-            <h3 class="artist-name">${a}</h3>
+            <h3 class="artist-name">${n}</h3>
             <p class="artist-bio">${o}</p>
             <button class="artist-more" type="button">Learn more</button>
           </div>
         </li>
-      `}).join("");artistsList.insertAdjacentHTML("beforeend",e)}function k(){L.classList.remove("is-hidden")}function T(){L.classList.add("is-hidden")}g();l.addEventListener("click",g);artistsList.addEventListener("click",t=>{const e=t.target.closest(".artist-more");if(!e)return;const n=e.closest(".artist-card");n&&openArtistModal(n.dataset.id)});const u=document.querySelector(".modal-content"),q=document.querySelector(".modal-close-btn"),i=document.querySelector(".backdrop"),h=document.getElementById("modalLoader");function P(t){const{strArtist:e,strArtistThumb:n,intFormedYear:a,intDiedYear:s,strGender:r,intMembers:o,strCountry:c,strBiographyEN:d,genres:E}=t;return`
+      `}).join("");artistsList.insertAdjacentHTML("beforeend",e)}function k(){h.classList.remove("is-hidden")}function T(){h.classList.add("is-hidden")}g();l.addEventListener("click",g);const u=document.querySelector(".modal-content"),q=document.querySelector(".modal-close-btn"),i=document.querySelector(".backdrop"),L=document.getElementById("modalLoader");function P(t){const{strArtist:e,strArtistThumb:a,intFormedYear:n,intDiedYear:s,strGender:r,intMembers:o,strCountry:c,strBiographyEN:d,genres:E}=t;return`
     <h2 class="artist-name">${e}</h2>
 
 <div class="artist-container">
   <div class="artist-image">
-    <img src="${n}" alt="${e||"Artist"}" />
+    <img src="${a}" alt="${e||"Artist"}" />
   </div>
 
   <div class="artist-info">
     <div class="info-row">
       <span class="label">Years active</span>
-      <span class="value">${a}-${s===null?"present":s}</span>
+      <span class="value">${n}-${s===null?"present":s}</span>
     </div>
 
     <div class="info-row">
@@ -56,5 +56,5 @@ import{a as v}from"./assets/vendor-D5GkNzM3.js";(function(){const e=document.cre
 
   `}async function C(t){i.classList.remove("is-hidden"),document.body.style.overflow="hidden";try{b();const e=await I(t);u.innerHTML=P(e)}catch(e){console.log(e),u.innerHTML=`
       <p class="error">Failed to load artist information</p>
-    `}finally{O()}}function m(){i.classList.add("is-hidden"),document.body.style.overflow="",u.innerHTML=""}q.addEventListener("click",m);document.addEventListener("keydown",t=>{t.key==="Escape"&&m()});i.addEventListener("click",t=>{t.target===i&&m()});function b(){h.classList.remove("is-hidden")}function O(){h.classList.add("is-hidden")}document.getElementById("artistsList");artistsList.addEventListener("click",async t=>{if(t.target.classList.contains("artist-more")){const e=t.target.closest(".artist-card");if(!e)return;const n=e.dataset.id;i.classList.remove("is-hidden"),document.body.style.overflow="hidden";const a=document.querySelector(".modal-content");a.innerHTML="",b();try{await C(n)}catch(s){console.log(s)}}});
+    `}finally{O()}}function m(){i.classList.add("is-hidden"),document.body.style.overflow="",u.innerHTML=""}q.addEventListener("click",m);document.addEventListener("keydown",t=>{t.key==="Escape"&&m()});i.addEventListener("click",t=>{t.target===i&&m()});function b(){L.classList.remove("is-hidden")}function O(){L.classList.add("is-hidden")}document.getElementById("artistsList");artistsList.addEventListener("click",async t=>{if(t.target.classList.contains("artist-more")){const e=t.target.closest(".artist-card");if(!e)return;const a=e.dataset.id;i.classList.remove("is-hidden"),document.body.style.overflow="hidden";const n=document.querySelector(".modal-content");n.innerHTML="",b();try{await C(a)}catch(s){console.log(s)}}});
 //# sourceMappingURL=index.js.map
